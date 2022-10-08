b818f04a26b241cfe67f69ac9943e1a7b69dbbc1
xand974 <xand974@gmail.com>
2022-10-08 22:18:17 +0200

    move to yarn

--- front/App.tsx
+++ front/App.tsx
@@ -1,4 +1,3 @@
-import { StyleSheet, View, Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import { store } from "./context/store";
@@ -8,25 +7,13 @@ import { useFonts } from "expo-font";
 import "react-native-gesture-handler";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ ...fonts });
-
-  if (!fontsLoaded) {
-    return (
-      <View>
-        <Text>Loading</Text>
-      </View>
-    );
-  }
+  useFonts({ ...fonts });
 
   return (
     <Provider store={store}>
       <SafeAreaProvider>
-        <MainRoutes></MainRoutes>
+        <MainRoutes />
       </SafeAreaProvider>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {},
-});
